a3d0cd39c5abbd643af65789742692f26060c743
Joshua Hallee <jthallee@email.neit.edu>
2020-01-06 22:00:41 -0500

    init commit

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+require('dotenv/config')
+const express = require('express')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+const app = express()
+const port = process.env.PORT_LOCAL
+
+//import routes
+const taskRoute = require('./routes/task')
+
+//middleware
+app.use(bodyParser.json())
+app.use('/api/task', taskRoute)
+
+// local mongoDB connection
+mongoose.connect(process.env.DB_CONNECTION_LOCAL, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log(`Connected to ${process.env.DB_CONNECTION_LOCAL}`);
+})
+
+// mongoDB listening for error while connected
+mongoose.connection.on('error', err => {
+    console.log(err)
+})
+
+app.get('/', (req, res) => {
+    res.send(`${res.statusCode}`)
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`)
+})
