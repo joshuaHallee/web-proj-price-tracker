51477b9626663a8bb4802993f7af045487604c31
Joshua Hallee <31211978+joshuaHallee@users.noreply.github.com>
2020-03-11 22:10:00 -0400

    add site module support

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -4,43 +4,26 @@ const cheerio = require('cheerio')
 const Search = require('../models/Search')
 const router = express.Router()
 
-router.get('/', async(req, res) => {
-    try {
-        res.send(`${res.statusCode} search!`)
-    } catch (err) {
-        res.status(500).json({ message: err })
-    }
-})
+// require site configurations
+const amazon = require('../sites/amazon');
+
+// init scrape method
+async function scrapePage(requestUrl) {
+  const html = await amazon.getHTML(requestUrl);
+  const amazonPrice = await amazon.getAmazonPrice(html);
+  console.log(`The price is ${amazonPrice}`);
+}
 
 router.post('/', async(req, res) => {
-    websiteUrl = req.body.websiteUrl
 
-    let config = {
-        headers: {
-            "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36",
-            "Accept-Language": "en-US,en;q=0.5",
-            "Referer": "https://www.google.com/"
-        }
-    }
+    requestUrl = req.body.websiteUrl
 
-    getUrl(websiteUrl, config)
-})
-
-async function getUrl(websiteUrl, config) {
     try {
-        axios.get(
-            `${websiteUrl}`,
-            config
-        )
-        .then(function (response) {
-            console.log(response)
-        })
-        .catch(function (error) {
-            console.log(error)
-        })
+      scrapePage(requestUrl)
     } catch (err) {
-        res.status(500).json({ message: err })
+      console.log(err)
     }
-}
+    
+})
 
 module.exports = router
